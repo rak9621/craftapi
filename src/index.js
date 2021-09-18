815e195d950a5741c7c01e4927fb882436424656
rak9621 <rakeshbaghel9621@gmail.com>
2021-09-18 06:05:05 +0530

    adding cors

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require("express")
-const request = require("request");
+const request = require("request")
+const cors = require('cors')
 require('../db/conn')
 const port = process.env.PORT || 3000
 
@@ -10,6 +11,7 @@ const app = express()
 const Craft = require('../db/model/model')
 
 app.use(express.json())
+app.use(cors())
 
 app.use('/api',craftApi)
 
